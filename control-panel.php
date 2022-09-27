ba80bcf3b3e90d4281899dfefcf8244306d16e48
Danirali <74063814+Danirali@users.noreply.github.com>
2022-09-27 21:33:45 +0100

    Update control-panel.php

--- control-panel.php
+++ control-panel.php
@@ -17,13 +17,22 @@ if ($_GET['stop']) {
 
 <h1>Server Controls</h1>
 
+<br>
+<h2 class="server-ip-label">Server IP: <? echo $_SERVER["REMOTE_ADDR"]; ?></h2>
+
+    <p class="center-txt check-status"><img border="0" alt="" src="status/check-status.php" /> <a>Java is running</a></p>
+
 <div class="center">
-  <div class="box">
+  <div class="container">
     <a class="start-btn" href="?start=true">Start Server</a>
   </div>
   <p class="space"></p>
-  <div class="box">
+  <div class="container">
     <a class="stop-btn" href="?stop=true">Stop Server</a>
   </div>
   <p class="space"></p>
 </div>
+
+<footer id="bottom">
+  <a class="site-byline" href="https://github.com/Danirali/Minecraft-Server-Status">Minecraft-Server-Status by Danirali</a>
+</footer>
